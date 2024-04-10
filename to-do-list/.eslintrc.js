d6f45ffad7295d9e00df1f9787e937c18510a658
unknown <titi.mariani.rm@gmail.com>
2024-04-10 11:51:29 -0300

    create project to do list

---
+++ to-do-list/.eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb',
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: [
+        '.eslintrc.{js,cjs}',
+      ],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'react/jsx-filename-extension': 'off',
+    'react/state-in-constructor': 'off',
+    'jsx-a11y/control-has-associated-label': 'off',
+    'react/forbid-prop-types': 0,
+  },
+};
